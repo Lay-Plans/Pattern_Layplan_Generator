ebd7bc65cabe112afc0429df850c4173182cb9a5
Lay-Plans <gudeggfashion@gmail.com>
2025-06-12 19:39:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, render_template, jsonify
+from generate_pattern import generate_hoodie_pattern
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.json  # incoming measurements
+    pattern = generate_hoodie_pattern(data)  # call your code
+    return jsonify(pattern.to_dict(orient="records"))
